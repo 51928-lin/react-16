13f2b795171ec9b3e30457fc17f5ace2a15815a4
Adam Yang <yangyitao100@sina.com>
2023-01-08 16:18:47 +0800

    feat: createElement代码实现

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@
 /*********react 18 end********/
 
 /*********react 18之前 start********/
-// import React from 'react';
-import React from './react';
-import ReactDOM from 'react-dom';
-ReactDOM.render(<div>Simple React App</div>, document.getElementById('root'))
+import React from 'react';
+// import React from './react';
+// import ReactDOM from 'react-dom';
+// ReactDOM.render(<div>Simple React App</div>, document.getElementById('root'))
 const element = <div class='class001'>Simple React App<span>xx1</span><span>xx2</span></div>
-
 console.log(element)
 /*********react 18之前 end********/
--- src/react.js
+++ src/react.js
@@ -1,13 +1,12 @@
+import { REACT_ELEMENT } from './utils'
+
 function createElement(type, properties = {}, children) {
-    let ref = properties.ref; // 后面会讲到，这里只需要知道是跟操作DOM相关
-    let key = properties.key; // 后面会讲到，这里只需要知道这个跟DOM DIFF相关
-
-    ;['ref', 'key'].forEach(key => { // 可能还会有别的属性也不需要，在发现的时候我们再添加需要删除的属性
-        if(properties[key]){
-            delete properties[key] // props中有些属性并不需要
-        }
+    let ref = properties.ref || null; // 后面会讲到，这里只需要知道是跟操作DOM相关
+    let key = properties.key || null; // 后面会讲到，这里只需要知道这个跟DOM DIFF相关
+    // 观察一下我们编写的createElement函数的返回值会发现有多余的__sorce,__self
+    ;['ref', 'key', '__self', '__source'].forEach(key => { // 可能还会有别的属性也不需要，在发现的时候我们再添加需要删除的属性
+        delete properties[key] // props中有些属性并不需要
     })
-
     let props = {...properties}
 
     if (arguments.length > 3) {
@@ -15,15 +14,16 @@ function createElement(type, properties = {}, children) {
         props.children = Array.prototype.slice.call(arguments, 2);
       } else {
         // 单个子元素，转化为数组
-        props.children = children ? [children] : [];
+        props.children = children;
       }
 
 
     return {
-      type, // 虚拟DOM的元素类型
-      ref,
-      key,
-      props
+        $$typeof: REACT_ELEMENT, // 代表着这是React元素，也就是React框架中的虚拟DOM
+        type, // 虚拟DOM的元素类型
+        ref,
+        key,
+        props
     }
   }
   const React = {
