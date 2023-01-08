c6425978bf024ff09962cfa973aecd55bf6bb7ba
Adam Yang <yangyitao100@sina.com>
2023-01-08 23:13:34 +0800

    feat: render/mount/createDOM

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-/*********react 18 start********/
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<div>Simple React App</div>);
-/*********react 18 end********/
-
-/*********react 18之前 start********/
-import React from 'react';
-// import React from './react';
-// import ReactDOM from 'react-dom';
-// ReactDOM.render(<div>Simple React App</div>, document.getElementById('root'))
-const element = <div class='class001'>Simple React App<span>xx1</span><span>xx2</span></div>
-console.log(element)
-/*********react 18之前 end********/
+import React from './react';
+import ReactDOM from './react-dom';
+ReactDOM.render(<div>Simple React App<span>xx1</span><span>xx2</span></div>, document.getElementById('root'))
