67f666b7be8748da23433306c769bcb7b2a713da
Adam Yang <yangyitao100@sina.com>
2023-01-08 11:50:14 +0800

    feat: 旧版本的render函数和旧版本的jsx转化

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+/*********react 18 start********/
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<div>Simple React App</div>);
+/*********react 18 end********/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>Simple React App</div>);
+/*********react 18之前 start********/
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(<div>Simple React App</div>, document.getElementById('root'))
+/*********react 18之前 end********/
