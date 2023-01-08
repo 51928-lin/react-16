6250dafa9418488bf8e534b8a19f031a7e1b3d6d
Adam Yang <yangyitao100@sina.com>
2023-01-08 11:11:57 +0800

    feat: delete useless code

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>Simple React App</div>);
+let element = <div>Simple React App</div>
+root.render(element);
