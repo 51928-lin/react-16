2cef4ddd070a2e1ae4c341243f83f4039f7152ea
yangyitao <yangyitao2@tal.com>
2023-02-06 21:15:21 +0800

    feat: 函数组件与类组件的基本调试

--- src/index.js
+++ src/index.js
@@ -4,33 +4,43 @@
 import React from './react';//////
 import ReactDOM from './react-dom';//////
 
+function MyFunctionComponent(){
+    return <div style={{
+        cursor: 'pointer', 
+        border: '1px solid gray', 
+        borderRadius: '6px',
+        width: '176px',
+        marginTop: '20px',
+        padding: '6px 12px'
+    }}>Hello Function Component</div>
+}
+
 class MyClassComponent extends React.Component{
     counter = 0
     isBlue = false
     constructor(props) {
         super(props);
-        this.state = { count: '0' };
-        this.myRef = React.createRef();
+        this.state = { text: '当前数字: 0' };
     }
-    updateShowText(newText){
+    updateShowText(){
         this.setState({
-            count: newText
+            text: '当前数字:' + ++this.counter
         })
-        const element = this.myRef.current;
-        element.style.color = this.isBlue ? 'blue' : 'red' //////
-        this.isBlue = !this.isBlue //////
     }
     render(){
-        return <div className='test-class' ref={ this.myRef } style={
-            {
-                color: 'red', 
-                cursor: 'pointer', 
-                border: '1px solid gray', 
-                borderRadius: '6px',
-                display: 'inline-block',
-                padding: '6px 12px'
-            }
-        } onClick={ () => this.updateShowText('' + ++this.counter) }>Simple React Counter: {this.state.count}</div>
+        return <div>
+                <div className='test-class' style={
+                    {
+                        color: 'red', 
+                        cursor: 'pointer', 
+                        border: '1px solid gray', 
+                        borderRadius: '6px',
+                        display: 'inline-block',
+                        padding: '6px 12px'
+                    }
+                } onClick={ () => this.updateShowText() }>Hello Class Component: {this.state.text}</div>
+                <MyFunctionComponent/>
+              </div>
     }
 }
 ReactDOM.render(<MyClassComponent />, document.getElementById('root'))
