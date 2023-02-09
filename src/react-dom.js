ed1a599698b807397cfb259b58ed5fa1f48a8658
yangyitao <yangyitao2@tal.com>
2023-02-09 22:49:42 +0800

    feat: dom-diff

--- src/react-dom.js
+++ src/react-dom.js
@@ -1,31 +1,31 @@
 import { REACT_ELEMENT, REACT_FORWARD_REF } from './utils'
 import { addEvent } from './event'
-function render(VNode, containerDOM){
+function render(VNode, containerDOM) {
     mount(VNode, containerDOM)
 }
-function mount(VNode, containerDOM){
+function mount(VNode, containerDOM) {
     let newDOM = createDOM(VNode)
     newDOM && containerDOM.appendChild(newDOM);
 }
-export function createDOM(VNode){
-    if(!VNode) return
-    const {type, props, ref} = VNode
+export function createDOM(VNode) {
+    if (!VNode) return
+    const { type, props, ref } = VNode
     let dom;
-    if (typeof type === 'function' && type.IS_CLASS_COMPONENT && VNode.$$typeof === REACT_ELEMENT){
+    if (typeof type === 'function' && type.IS_CLASS_COMPONENT && VNode.$$typeof === REACT_ELEMENT) {
         return getDomByClassComponent(VNode)
-    } else if (typeof type === 'function' && VNode.$$typeof === REACT_ELEMENT){
+    } else if (typeof type === 'function' && VNode.$$typeof === REACT_ELEMENT) {
         return getDomByFunctionComponent(VNode)
     } if (type && type.$$typeof === REACT_FORWARD_REF) {
         return getDomByRefForwardFunction(VNode);
     } else if (type && VNode.$$typeof === REACT_ELEMENT) {
         dom = document.createElement(type);
-    } 
-    if(props){
+    }
+    if (props) {
         if (typeof props.children === 'object' && props.children.type) {
             mount(props.children, dom)
         } else if (Array.isArray(props.children)) {
             mountArray(props.children, dom);
-        } else if (typeof props.children === 'string'){
+        } else if (typeof props.children === 'string') {
             dom.appendChild(document.createTextNode(props.children));
         }
     }
@@ -34,19 +34,19 @@ export function createDOM(VNode){
     ref && (ref.current = dom)
     return dom
 }
-function mountArray(children, parent){
-    if(!Array.isArray(children)) return
+function mountArray(children, parent) {
+    if (!Array.isArray(children)) return
     for (let i = 0; i < children.length; i++) {
-        if (typeof children[i] === 'string'){
+        if (typeof children[i] === 'string') {
             parent.appendChild(document.createTextNode(children[i]))
-        }else{
+        } else {
             mount(children[i], parent)
         }
     }
 }
 
 function setPropsForDOM(dom, VNodeProps = {}) {
-    if(!dom) return
+    if (!dom) return
     for (let key in VNodeProps) {
         if (key === 'children') continue;
         if (/^on[A-Z].*/.test(key)) { // 大家需要注意这个正则表达式为什么这么写 
@@ -78,7 +78,7 @@ function getDomByFunctionComponent(vNode) {
     if (!renderVNode) return null;
     return createDOM(renderVNode);
 }
-function getDomByClassComponent(vNode){
+function getDomByClassComponent(vNode) {
     let { type, props, ref } = vNode;
     let instance = new type(props)
     ref && (ref.current = instance);
@@ -87,21 +87,83 @@ function getDomByClassComponent(vNode){
     if (!renderVNode) return null;
     return createDOM(renderVNode);
 }
-function getDomByRefForwardFunction(vNode){
+function getDomByRefForwardFunction(vNode) {
     let { type, props, ref } = vNode;
     let renderVdom = type.render(props, ref);
     if (!renderVdom) return null;
     return createDOM(renderVdom);
 }
-export function updateDomTree(oldDOM, newVNode){
-    if(!oldDOM) return
-    let parentNode = oldDOM.parentNode
-    parentNode.removeChild(oldDOM);
-    parentNode.appendChild(createDOM(newVNode))
+function updateProps(currentDOM, oldVNodeProps, newVNodeProps){
+
 }
-export function findDomByVNode(VNode){
-    if(!VNode) return
-    if(VNode.dom) return VNode.dom
+function updateChildren(currentDOM, oldVNodeChildren, newVNodeChildren){
+
+}
+function updateClassComponent(oldVNode, newVNode){
+
+}
+function updateFunctionComponent(oldVNode, newVNode){
+    
+}
+function deepDOMDiff(oldVNode, newVNode) {
+    let diffTypeMap = {
+        ORIGIN_NODE: typeof oldVNode.type === 'string', // 原生节点
+        CLASS_COMPONENT: typeof oldVNode.type === 'function' && oldVNode.type.isReactComponent,
+        FUNCTION_COMPONENT: typeof oldVNode.type === 'function'
+    }
+    let DIFF_TYPE = Object.keys(diffTypeMap).filter(key => diffTypeMap[key])[0]
+    switch(DIFF_TYPE){
+        case 'ORIGIN_NODE':
+            let currentDOM = newVNode.dom = findDomByVNode(oldVNode);
+            updateProps(currentDOM, oldVNode.props, newVNode.props);
+            updateChildren(currentDOM, oldVNode.props.children, newVNode.props.children);
+            break;
+        case 'CLASS_COMPONENT':
+            updateClassComponent(oldVNode, newVNode);
+            break;
+        case 'FUNCTION_COMPONENT':
+            updateFunctionComponent(oldVNode, newVNode);
+            break;
+        default:
+            break;
+    }
+}
+function removeVNode(vNode) {
+    const currentDOM = findDomByVNode(vNode);
+    if (currentDOM) currentDOM.remove();
+}
+// 开始dom-diff
+export function updateDomTree(oldVNode, newVNode, oldDOM) {
+    const typeMap = {
+        NO_OPERATE: !oldVNode && !newVNode,
+        ADD: !oldVNode && newVNode,
+        DELETE: oldVNode && newVNode,
+        REPLACE: oldVNode && newVNode && oldVNode.type !== newVNode.type // 类型不同
+    }
+    let UPDATE_TYPE = Object.keys(typeMap).filter(key => typeMap[key])[0]
+
+    switch(UPDATE_TYPE){
+        case 'NO_OPERATE':
+            break
+        case 'DELETE':
+            removeVNode(oldVNode);
+            break
+        case 'ADD':
+            oldDOM.parentNode.appendChild(createDOM(newVNode));
+            break
+        case 'REPLACE':
+            removeVNode(oldVNode);
+            // 这里直接追加到尾巴上
+            oldDOM.parentNode.appendChild(createDOM(newVNode));
+            break
+        default:
+            // 深度的 dom-diff，新老虚拟DOM都存在且类型相同
+            deepDOMDiff(oldVNode, newVNode)
+    }
+}
+export function findDomByVNode(VNode) {
+    if (!VNode) return
+    if (VNode.dom) return VNode.dom
 }
 const ReactDOM = {
     render
